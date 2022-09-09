aab38765b3b1844c8311f0b28884818c3338571a
Luis Rivera <r23felipe@gmail.com>
2022-09-09 17:56:08 -0500

    Widfly y script base de datos

---
+++ extras/test-nexos.sql
@@ -0,0 +1,92 @@
+CREATE DATABASE  IF NOT EXISTS `test-nexos` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `test-nexos`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: test-nexos
+-- ------------------------------------------------------
+-- Server version	5.7.37-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `departamentos`
+--
+
+DROP TABLE IF EXISTS `departamentos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `departamentos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `DEPARTAMENTO_CODIGO` varchar(45) NOT NULL,
+  `FECHA_HORA_CREA` datetime NOT NULL,
+  `FECHA_HORA_MODIFICA` datetime DEFAULT NULL,
+  `DEPARTAMENTO_NOMBRE` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departamentos`
+--
+
+LOCK TABLES `departamentos` WRITE;
+/*!40000 ALTER TABLE `departamentos` DISABLE KEYS */;
+INSERT INTO `departamentos` VALUES (1,'02','2022-09-09 16:45:00','2022-09-09 17:08:06','VALLE DEL CAUCA');
+/*!40000 ALTER TABLE `departamentos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleados` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `direccion` varchar(45) NOT NULL,
+  `ciudad` varchar(45) NOT NULL,
+  `fecha_hora_crea` datetime NOT NULL,
+  `fecha_hora_modifica` datetime DEFAULT NULL,
+  `documento_numero` varchar(16) NOT NULL,
+  `documento_tipo` varchar(3) NOT NULL,
+  `correo_electronico` varchar(45) NOT NULL,
+  `nombres` varchar(45) NOT NULL,
+  `telefono` varchar(16) NOT NULL,
+  `apellidos` varchar(45) NOT NULL,
+  `departamentos_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKnk7utcnyuo52i2uauqhirnu8i` (`departamentos_id`),
+  CONSTRAINT `FKnk7utcnyuo52i2uauqhirnu8i` FOREIGN KEY (`departamentos_id`) REFERENCES `departamentos` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleados`
+--
+
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` VALUES (2,'1','2','2022-09-09 16:58:20','2022-09-09 16:58:28','1','RC','2','1','1','1',1);
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-09 17:50:28
